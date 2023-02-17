dcca7d67657741affbb12ccad73f5268cf0096e6
Build Er <builder@macmini6.local>
2023-02-16 23:52:10 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXStandardClasses",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXStandardClasses",
+			targets: ["GXStandardClassesWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXStandardClassesWrapper",
+				dependencies: [
+					"GXStandardClasses",
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS, .tvOS, .watchOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS])),
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXStandardClasses",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "99a59bb61bfb466c1d3b175be7f031f9de7e320da1344978c2f06bf9fd0ed252"
+		)
+	]
+)
