fb49ab8250d5f1d829172b5ed288965cc208170b
Build Er <builder@macmini6.local>
2023-05-02 10:21:23 -0300

    Package version: 1.0.0-beta+20230502131235

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 		.target(name: "GXStandardClassesWrapper",
 				dependencies: [
 					"GXStandardClasses",
-					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.watchOS, .tvOS, .iOS])),
 					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS])),
 					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS])),
 					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
@@ -27,8 +27,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXStandardClasses",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.0.0-beta+20230428202437.xcframework.zip",
-			checksum: "f351dd2fb415d0ed503b55fea39b0fd33bce795e2bfa9e2c34f4a039c90e610b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.0.0-beta+20230502131235.xcframework.zip",
+			checksum: "8264b1cd3706642338be8dc09529cc5e0a80891ae52e6f92c66f3b7e5b8896b5"
 		)
 	]
 )
