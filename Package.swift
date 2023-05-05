92fa66c057dfc93064258113114f1ebacdded62b
Build Er <builder@macmini6.local>
2023-05-05 09:45:40 -0300

    Package version: 1.0.0-beta+20230505123704

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXStandardClasses",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.0.0-beta+20230504210908.xcframework.zip",
-			checksum: "78e6b3da45eec6c3aec3e6109f7b709e017e9b76e53024b24fdeeef17eaad021"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.0.0-beta+20230505123704.xcframework.zip",
+			checksum: "cac7d2a48b972186b24c09a1e20f8d7f3e1c2a82d0b04e8a0fdf6a0abdef6eca"
 		)
 	]
 )
