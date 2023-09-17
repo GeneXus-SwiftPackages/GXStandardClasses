de7e057255617deaf6aec1bdea4b3d3eb757dc04
genexusbot <builder@genexus.com>
2023-09-17 16:18:03 -0300

    Package version: 1.2.0-beta.28

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXStandardClassesWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.2.0-beta.27")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.2.0-beta.28")
 	],
 	targets: [
 		.target(name: "GXStandardClassesWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXStandardClasses",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.2.0-beta.27.xcframework.zip",
-			checksum: "5533b5b030bc7da4ead4e5ed2129f03473a1a5fdd07d18203ba1f8e7fe8965de"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXStandardClasses-1.2.0-beta.28.xcframework.zip",
+			checksum: "6acce82da94193cd1bf4fb968e39c1d458acc7d5e4537401276b5d8882b0e985"
 		)
 	]
 )
