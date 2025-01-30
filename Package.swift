b2ad5a6d2526f8833b82c79528852fac32bf5a1d
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-30 01:09:08 -0300

    Package version: 3.0.0-rc.15

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
 	name: "GXStandardClasses",
-	platforms: [.iOS("13.0"), .watchOS("9.0"), .tvOS("13.0"), .visionOS("1.0")],
+	platforms: [.iOS("15.0"), .watchOS("10.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXStandardClasses",
 			targets: ["GXStandardClassesWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "3.0.0-rc.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "3.0.0-rc.15")
 	],
 	targets: [
 		.target(name: "GXStandardClassesWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXStandardClasses",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXStandardClasses-3.0.0-rc.8.xcframework.zip",
-			checksum: "d5e08d6a4a174ffb8f32649384933a78caf1c3b45dc5d2da814f6264d5305d17"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXStandardClasses-3.0.0-rc.15.xcframework.zip",
+			checksum: "bb32a24433d84671bd884f60c3e21dd7ea1b80f2385bbb794fbfcaed8b6a5e36"
 		)
 	]
 )
